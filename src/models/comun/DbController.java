6d06e2639c3dfff5bf277415052a83c81afe2254
DoberWho <jorge.parada@ctbarbanza.com>
2019-10-22 14:02:08 +0200

    DbController

--- src/models/comun/DbController.java
+++ src/models/comun/DbController.java
@@ -8,11 +8,13 @@ import java.sql.Statement;
 
 public class DbController {
 	
+	private Connection con;
+	
 	private static DbController instance;
-
+	 
 	private DbController() { 
 		DbSqlite db = DbSqlite.getInstance();
-		Connection con = db.getConnection();
+		con = db.getConnection();
 	}
 
 	public static DbController getInstance() {
@@ -22,28 +24,30 @@ public class DbController {
 		return instance;
 	}
 	
-	public static boolean saveDb(Connection con, String nombre) { 
+	public boolean saveDb(String TABLA, String campo, String valor) { 
+		
+		String sqlExist = "select * from '"+TABLA+"' where '"+campo+"' = '"+valor+"'";
+		String sqlIns = "INSERT INTO "+TABLA+" ('"+campo+"') VALUES ('"+valor+"');";
 		
 		try {
 			
-			Statement statemnt = con.createStatement();
+			Statement statemnt = this.con.createStatement();
 			
-			String sql = "select * from categorias where nombre = '"+nombre+"'";
-			ResultSet res = statemnt.executeQuery(sql);
+			
+			ResultSet res = statemnt.executeQuery(sqlExist);
 			if (res.next()) {
-				System.out.println("Categoria "+nombre+" Ya Existe");
+				System.out.println("Objeto "+TABLA+" - "+valor+" Ya Existe");
 				return false;
-			}
+			} 
 			
-			String insertar = "INSERT INTO 'categorias' ('nombre') VALUES ('"+nombre+"');";
-			statemnt.execute(insertar);
+			statemnt.execute(sqlIns);
 		 
 		} catch (SQLException e) { 
 			e.printStackTrace();
 			System.out.println(e);
 			return false;
 		}
-		System.out.println("Categoria "+nombre+" Insertada");
+		System.out.println("Objeto "+TABLA+" - "+valor+" Insertada");
 		return true;
 	}
 
