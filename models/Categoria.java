aca1b9a130d08b1c49d9bfbf8d3d52fbabe5fb24
DoberWho <jorge.parada@ctbarbanza.com>
2019-10-22 13:22:28 +0200

    Modelos

---
+++ models/Categoria.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.Date;
+
+public class Categoria {
+
+	private Integer id;
+	private Date created;
+	private String nombre;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
