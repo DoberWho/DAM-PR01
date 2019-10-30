355467b641c5e9b0d82a4b1d99d8365b1c54b943
DoberWho <jorge.parada@ctbarbanza.com>
2019-10-30 13:59:39 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,27 @@ import src.models.comun.DbObject;
 import src.models.comun.Tools; 
 
 public class Main {
+	
+	/**
+	 * <strong>EJERCICIOS</strong>
+	 * <strong>Entrega: 6 de Noviembre (Github)</strong>
+	 * 
+	 * 1.- Actualizar vuestros repositorio con este
+	 * 2.- Interface Comandos:
+	 * 	-> Categoria: CRUD
+	 *  -> Producto: CRUD // NOTA: Un producto tiene un id_Categoria obligatorio, y tiene existir la categoria
+	 *  -> Cliente: CRUD
+	 * 
+	 * 2.- Listar:
+	 *  -> Productos de una Categoria // Dado un ID de categoria; tener todos los productos
+	 * 
+	 * 3*.- Validaciones:
+	 *  -> Los datos del C/U (Create/Update) tienen que cumplir condiciones coherentes. 
+	 *  => PISTA: regex
+	 * 
+	 * *-> Para nota ;)
+	 * 
+	 */
 
 	public static void main(String[] args) {
 		
